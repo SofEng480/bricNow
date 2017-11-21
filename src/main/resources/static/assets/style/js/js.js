5e015f81d180d334335b5fd37dd035d7fdb9c127
Byung Hun Lee <byunghunlee@cpp.edu>
2017-11-21 03:03:26 -0800

    Changed front page according to meeting on 11/18

--- src/main/resources/static/assets/style/js/js.js
+++ src/main/resources/static/assets/style/js/js.js
@@ -1,24 +1,41 @@
-    function openTab(evt, tabName){
-        var i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("tabcontent");            
-        for(i=0; i<tabcontent.length; i++){
-            tabcontent[i].style.display="none";
-        }
-
-        tablinks = document.getElementsByClassName("tablinks");
-        for(i=0; i<tablinks.length; i++){
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-
-        document.getElementById(tabName).style.display = "block";
-        evt.currentTarget.className += " active";
+function openTab(evt, tabName){
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");            
+    for(i=0; i<tabcontent.length; i++){
+        tabcontent[i].style.display="none";
     }
-    document.getElementById("default").click();
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for(i=0; i<tablinks.length; i++){
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }  
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+document.getElementById("default").click();
+
+function openTab2(evt, tabName){
+    var i, tabcontent2, verticaltablinks;
+    tabcontent2 = document.getElementsByClassName("tabcontent2");            
+    for(i=0; i<tabcontent2.length; i++){
+        tabcontent2[i].style.display="none";
+    }
+
+    verticaltablinks = document.getElementsByClassName("verticaltablinks");
+    for(i=0; i<verticaltablinks.length; i++){
+        verticaltablinks[i].className = verticaltablinks[i].className.replace(" active", "");
+    }  
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+document.getElementById("default2").click();
 
 google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.load('current', {packages: ['gauge']});
 google.charts.setOnLoadCallback(drawBasic);
-google.charts.setOnLoadCallback(drawBasic2);
-google.charts.setOnLoadCallback(drawBasic3);
+google.charts.setOnLoadCallback(drawMeter);
 
 function drawBasic() {
 
@@ -50,77 +67,33 @@ function drawBasic() {
         }
     };
 
-    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+    var chart = new google.visualization.LineChart(document.getElementById('bricLaterGraph1'));
 
     chart.draw(data, options);
 }
 
-function drawBasic2() {
+function drawMeter(){
+    
+  var data = google.visualization.arrayToDataTable([
+          ['Label', 'Value'],
+          ['Capacity', 80]
+        ]);
 
-    var data = new google.visualization.DataTable();
-    data.addColumn('number', 'X');
-    data.addColumn('number', 'Dogs');
+        var options = {
+            width: 600, height: 300,
+            yellowFrom:75, yellowTo: 90,
+            redFrom: 90, redTo: 100,
+            minorTicks: 5
+            
+        };
 
-    data.addRows([
-        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
-        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
-        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
-        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
-        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
-        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
-        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
-        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
-        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
-        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
-        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
-        [66, 70], [67, 72], [68, 75], [69, 80]
-    ]);
+        var chart = new google.visualization.Gauge(document.getElementById('speedmeter'));
 
-    var options = {
-        hAxis: {
-          title: 'Time'
-        },
-        vAxis: {
-          title: 'Popularity'
-        }
-    };
+        chart.draw(data, options);
 
-    var chart = new google.visualization.LineChart(document.getElementById('chart_div2'));
-
-    chart.draw(data, options);
+        setInterval(function() {
+          data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+          chart.draw(data, options);
+        }, 10000);
 }
-
-function drawBasic3() {
-
-    var data = new google.visualization.DataTable();
-    data.addColumn('number', 'X');
-    data.addColumn('number', 'Dogs');
-
-    data.addRows([
-        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
-        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
-        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
-        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
-        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
-        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
-        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
-        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
-        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
-        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
-        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
-        [66, 70], [67, 72], [68, 75], [69, 80]
-    ]);
-
-    var options = {
-        hAxis: {
-          title: 'Time'
-        },
-        vAxis: {
-          title: 'Popularity'
-        }
-    };
-
-    var chart = new google.visualization.LineChart(document.getElementById('chart_div3'));
-
-    chart.draw(data, options);
-}
+    
