0e90d526ab84c6f4f241805ea35a5631ee94fb2e
Byung Hun Lee <byunghunlee@cpp.edu>
2017-11-26 17:32:15 -0800

    Added graph for each day

--- src/main/resources/static/assets/style/js/js.js
+++ src/main/resources/static/assets/style/js/js.js
@@ -32,12 +32,12 @@ function openVerticalTab(evt, tabName){
     evt.currentTarget.className += " active";
 }
 
-//Function for drawing line graph
-function drawLineGraph() {
-
+//Function for drawing monday graph
+function drawMondayGraph() {
+    
     var data = new google.visualization.DataTable();
     data.addColumn('number', 'X');
-    data.addColumn('number', 'Dogs');
+    data.addColumn('number', 'Person');
 
     data.addRows([
         [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
@@ -59,7 +59,7 @@ function drawLineGraph() {
           title: 'Time'
         },
         vAxis: {
-          title: 'Popularity'
+          title: 'Population'
         }
     };
 
@@ -67,6 +67,223 @@ function drawLineGraph() {
 
     chart.draw(data, options);
 }
+
+//Function for drawing tuesday graph
+function drawTuesdayGraph() {
+    
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Person');
+
+    data.addRows([
+        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+        [66, 70], [67, 72], [68, 75], [69, 80]
+    ]);
+
+    var options = {
+        hAxis: {
+          title: 'Time'
+        },
+        vAxis: {
+          title: 'Population'
+        }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('bricLaterTuesdayGraph'));
+
+    chart.draw(data, options);
+}
+
+//Function for drawing wednesday graph
+function drawWednesdayGraph() {
+    
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Person');
+
+    data.addRows([
+        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+        [66, 70], [67, 72], [68, 75], [69, 80]
+    ]);
+
+    var options = {
+        hAxis: {
+          title: 'Time'
+        },
+        vAxis: {
+          title: 'Population'
+        }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('bricLaterWednesdayGraph'));
+
+    chart.draw(data, options);
+}
+
+//Function for drawing thursday graph
+function drawThursdayGraph() {
+    
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Person');
+
+    data.addRows([
+        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+        [66, 70], [67, 72], [68, 75], [69, 80]
+    ]);
+
+    var options = {
+        hAxis: {
+          title: 'Time'
+        },
+        vAxis: {
+          title: 'Population'
+        }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('bricLaterThursdayGraph'));
+
+    chart.draw(data, options);
+}
+
+//Function for drawing friday graph
+function drawFridayGraph() {
+    
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Person');
+
+    data.addRows([
+        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+        [66, 70], [67, 72], [68, 75], [69, 80]
+    ]);
+
+    var options = {
+        hAxis: {
+          title: 'Time'
+        },
+        vAxis: {
+          title: 'Population'
+        }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('bricLaterFridayGraph'));
+
+    chart.draw(data, options);
+}
+
+//Function for drawing saturday graph
+function drawSaturdayGraph() {
+    
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Person');
+
+    data.addRows([
+        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+        [66, 70], [67, 72], [68, 75], [69, 80]
+    ]);
+
+    var options = {
+        hAxis: {
+          title: 'Time'
+        },
+        vAxis: {
+          title: 'Population'
+        }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('bricLaterSaturdayGraph'));
+
+    chart.draw(data, options);
+}
+
+//Function for drawing sunday graph
+function drawSundayGraph() {
+    
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Person');
+
+    data.addRows([
+        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+        [66, 70], [67, 72], [68, 75], [69, 80]
+    ]);
+
+    var options = {
+        hAxis: {
+          title: 'Time'
+        },
+        vAxis: {
+          title: 'Population'
+        }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('bricLaterSundayGraph'));
+
+    chart.draw(data, options);
+}
+
 
 //Function for drawing speedmeter using google charts
 function drawSpeedMeter(){
@@ -130,8 +347,13 @@ window.onclick = function(event){
 
 google.charts.load('current', {packages: ['corechart', 'line']});
 google.charts.load('current', {packages: ['gauge']});
-google.charts.setOnLoadCallback(drawLineGraph);
+google.charts.setOnLoadCallback(drawMondayGraph);
+google.charts.setOnLoadCallback(drawTuesdayGraph);
+google.charts.setOnLoadCallback(drawWednesdayGraph);
+google.charts.setOnLoadCallback(drawThursdayGraph);
+google.charts.setOnLoadCallback(drawFridayGraph);
+google.charts.setOnLoadCallback(drawSaturdayGraph);
+google.charts.setOnLoadCallback(drawSundayGraph);
 google.charts.setOnLoadCallback(drawSpeedMeter);
 document.getElementById("default").click();
 document.getElementById("default2").click();
-createSpeedMeter();
