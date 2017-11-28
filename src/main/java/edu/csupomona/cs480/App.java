1b2b7d520303ff8bccf23b179ddc620ac6a64e73
Erick <elopezsolis@cpp.edu>
2017-11-28 10:00:18 -0800

    Deleted unused files and Cleaned code

--- src/main/java/edu/csupomona/cs480/App.java
+++ src/main/java/edu/csupomona/cs480/App.java
@@ -16,35 +16,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 public class App {
 
-    /**
-     * This is a good example of how Spring instantiates
-     * objects. The instances generated from this method
-     * will be used in this project, where the Autowired
- 4    * annotation is applied.
-     */
-    @Bean
-    public UserManager userManager() {
-        UserManager userManager = new FSUserManager();
-        return userManager;
-    }
-    
-    @Bean
-    public GpsProductManager gpsProductManager() {
-    	GpsProductManager gpsManager = new EBayGpsProductManager();
-        return gpsManager;
-    }
     @Bean
     public AreaManager areaManager(){
         AreaManager area =new BricAreaManager();
         return area;
     }
-    /**
-     * This is the running main method for the web application.
-     * Please note that Spring requires that there is one and
-     * ONLY one main method in your whole program. You can create
-     * other main methods for testing or debugging purposes, but
-     * you cannot put extra main method when building your project.
-     */
     public static void main(String[] args) throws Exception {
         // Run Spring Boot
         SpringApplication.run(App.class, args);
