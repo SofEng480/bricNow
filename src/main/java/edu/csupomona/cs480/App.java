dea27a2d9ff8fb1b0a71e37c44ca1fde6e3bb60f
Erick <elopezsolis@cpp.edu>
2017-11-28 19:50:42 -0800

    Added code to update automatically. Right now it runs at 30 seconds

--- src/main/java/edu/csupomona/cs480/App.java
+++ src/main/java/edu/csupomona/cs480/App.java
@@ -8,12 +8,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableJpaRepositories(basePackages = "edu.csupomona.cs480.Repo")
 @ComponentScan
 @SpringBootApplication
+@EnableScheduling
 public class App {
 
     @Bean
--- src/main/java/edu/csupomona/cs480/controller/WebController.java
+++ src/main/java/edu/csupomona/cs480/controller/WebController.java
@@ -6,12 +6,15 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.csupomona.cs480.Repo.AreaRepository;
 import edu.csupomona.cs480.data.Area;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -31,22 +34,22 @@ public class WebController {
 
     @Autowired
     private AreaRepository areaRepository;
-
-	@RequestMapping(value = "/recent" ,method = RequestMethod.GET)
-    public Map<String, List<Area>> getRecent() throws IOException {
+//	@RequestMapping(value = "/recent" ,method = RequestMethod.GET)
+    @Scheduled(fixedRate= 30000)
+    public void getRecent() throws IOException {
     	Map<String,List<Area>> map = new HashMap<>();
+        Random ran = new Random();
         ObjectMapper JSON = new ObjectMapper();
         map.put("areaPopulation",areaRepository.getMostRecent());
         File file = new File("src/main/resources/static/assets/data/bricNowData.json");
         FileWriter writer = new FileWriter(file);
         JSON.writeValue(file,map);
         writer.close();
-        return map;
+//        return map;
     }
     @RequestMapping(value = "/areas/{id}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public Area getSum(@PathVariable("id") int id){
         return areaRepository.getArea(id);
     }
-
 }
